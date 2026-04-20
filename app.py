fbe3343371f0bb428c032dcab616adacdd49223e
jordanandrevon <jordan.andrevon@gmail.com>
2026-04-20 14:46:58 +0000

    Test au 20/04

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-figure = px.pie(données, values='qte', names='produit', title='quantité vendue par produit')
+figure = px.pie(données, values=sum(prix) * sum(qte), names='produit', title='chiffe affaire par produit')
 
 figure.write_html('ventes-par-region.html')
 
